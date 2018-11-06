bf6c14fc04f1c70265c7c8a7d9a0a7d0bee934f9
Garten Choi <jedo0328@naver.com>
2018-11-06 10:08:36 +0900

    init

--- microbiology.py
+++ microbiology.py
@@ -1,29 +1,33 @@
 import csv
 
-#com1과 com2 비교
-def compare(com1,com2):
-    comf1=open('data/%s_species.csv'%com1,'r')
-    comf2=open('data/%s_species.csv'%com2,'r')
 
-    comd1={}
+# com1과 com2 비교
+def compare(com1, com2):
+    comf1 = open('data/%s_species.csv' % com1, 'r')
+    comf2 = open('data/%s_species.csv' % com2, 'r')
 
-    comw=open('result/%s_vs_%s.txt'%(com1,com2),'w',encoding='utf-8')
+    comd1 = {}
+
+    comw = open('result/%s_vs_%s.txt' % (com1, com2), 'w', encoding='utf-8')
 
     for line in csv.reader(comf1):
-        if line[2]=='Count':continue
+        if line[2] == 'Count': continue
         comd1[line[0]] = int(line[2])
 
     for line in csv.reader(comf2):
-        if line[2]=='Count':continue
+        if line[2] == 'Count': continue
         try:
-            ratio=comd1[line[0]]/int(line[2])
-            if ratio>1.3 or ratio<0.7:
-                comw.write('%s: %f만큼 변화함.\n'%(line[0],ratio-1))
+            ratio = comd1[line[0]] / int(line[2])
+            if ratio > 1.3 or ratio < 0.7:
+                comw.write('%s: %f만큼 변화함.\n' % (line[0], ratio - 1))
             del comd1[line[0]]
-        except:comw.write('%s: %s에서 새로 검출됨.\n'%(line[0],com2))
+        except:
+            comw.write('%s: %s에서 새로 검출됨.\n' % (line[0], com2))
     for key in comd1.keys():
-        comw.write('%s: %s에서 검출되지 않음\n'%(key,com2))
+        comw.write('%s: %s에서 검출되지 않음\n' % (key, com2))
 
-compare('con','dn')
-compare('dnex','con')
-compare('con','ex')
+
+if __name__ == "__main__":
+    compare('con', 'dn')
+    compare('dnex', 'con')
+    compare('con', 'ex')
